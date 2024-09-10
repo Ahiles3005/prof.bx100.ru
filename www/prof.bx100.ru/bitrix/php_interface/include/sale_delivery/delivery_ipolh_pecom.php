9a7ce87f8ff14552148b2893c1c46dd58bfcde79
Одногулов Руслан Хосянович <orh@cds.guru>
2024-09-10 15:33:30 +0300

    fix

--- www/prof.bx100.ru/bitrix/php_interface/include/sale_delivery/delivery_ipolh_pecom.php
+++ www/prof.bx100.ru/bitrix/php_interface/include/sale_delivery/delivery_ipolh_pecom.php
@@ -1,7 +1,7 @@
 <?
 CModule::IncludeModule('pecom.ecomm');
 
-if(file_exists($_SERVER['DOCUMENT_ROOT'].'/bitrix/modules/'.IPOLH_PECOM.'/classes/general/deliveryHandler.php')){
+if (defined('IPOLH_PECOM') && file_exists($_SERVER['DOCUMENT_ROOT'] . '/bitrix/modules/' . IPOLH_PECOM . '/classes/general/deliveryHandler.php')) {
     AddEventHandler("sale", "onSaleDeliveryHandlersBuildList", array('\Ipolh\Pecom\deliveryHandler', 'Init'));
 }
 ?>
